a93c3a98bc83c3a5a7685b5f4c19000eb8f06855
shanth96 <shanth.sathiyaseelan@shopify.com>
2025-02-18 14:23:05 -0500

    Release commit for 18.0.8-shopify-2
    Signed-off-by: shanth96 <shanth.sathiyaseelan@shopify.com>

--- go/vt/servenv/version.go
+++ go/vt/servenv/version.go
@@ -19,4 +19,4 @@ package servenv
 // THIS FILE IS AUTO-GENERATED DURING NEW RELEASES BY ./tools/do_releases.sh
 // DO NOT EDIT
 
-const versionName = "18.0.8-shopify-1"
+const versionName = "18.0.8-shopify-2"
