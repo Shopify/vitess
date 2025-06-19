1d62b3c99b9ec2f08881c236c055c6b7fd4b2a41
Tiffany Yeh <33907866+chenhuiyeh@users.noreply.github.com>
2025-06-19 11:44:37 -0400

    Migrate to observe errors

--- web/vtadmin/src/errors/bugsnag.ts
+++ web/vtadmin/src/errors/bugsnag.ts
@@ -26,7 +26,14 @@ const { VITE_BUGSNAG_API_KEY } = env();
  */
 export const initialize = () => {
     if (typeof VITE_BUGSNAG_API_KEY === 'string' && VITE_BUGSNAG_API_KEY.length) {
-        BugsnagJS.start(VITE_BUGSNAG_API_KEY);
+        BugsnagJS.start({ 
+            apiKey: VITE_BUGSNAG_API_KEY,
+            // Set endpoints to point to proxy
+            endpoints: {
+                notify: 'https://error-analytics-production.shopifysvc.com',
+                sessions: 'https://error-analytics-sessions-production.shopifysvc.com',
+            },
+        });
     }
 };
 
